c31d701f5170791ec7d80b73162fffaeafca5237
hernandezeacee <hernandezeaceec@gmail.com>
2025-12-18 15:28:58 +0800

    Create schema_v2.sql

---
+++ database/schema_v2.sql
@@ -0,0 +1,9 @@
+CREATE TABLE achievement (
+    achievement_id INT PRIMARY KEY,
+    pokemon_id INT NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    points_reward INT DEFAULT 0,
+    requirement INT DEFAULT 0,
+    FOREIGN KEY (pokemon_id) REFERENCES pokemon(pokemon_id)
+);
